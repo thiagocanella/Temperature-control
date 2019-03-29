a6ba759dcbded9320578d6f37def40c721c7904c
Thiago Canella <thiagocanella@gmail.com>
2019-03-28 23:33:39 -0300

    adição de limitador de tempo automatico

--- comunnication.py
+++ comunnication.py
@@ -4,9 +4,24 @@ import datetime
 import platform
 import os
 from shutil import copyfile
-
 ts = time.time()
 
+
+
+#DEFINIÇÕES DO EXPERIMENTO
+minima = 33.00
+maxima = 36.00
+horasDeExperimento = 4
+minutosDeExperimento = 0
+#----------------------------
+
+
+
+
+
+#FUNÇÕES CONVERSORAS
+def tempoTotalEmSegundos(hora,minuto):
+    return (hora * 3600) + (minuto * 60)
 def decisaoParaNumero(decisao):
     if decisao == "Alta":
         return 2
@@ -16,21 +31,19 @@ def decisaoParaNumero(decisao):
         return 0
     return 0
 
-
+#CONFIGURAÇÕES DE PORTA COM E FORMATO DE DATA/HORA PELO SISTEMA OPERACIONAL
 if platform.system() == "Windows":
     ser = serial.Serial('COM15', 9600, timeout=0)
     stamp = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y %H-%M-%S')
-
-
 if platform.system() == "Linux":
     ser = serial.Serial('/dev/ttyUSB0', 9600)
     stamp = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y %H:%M:%S')
 
+#MONTA ARQUIVO DE LOG
 try:
     os.remove('lastplot.plt')
 except FileNotFoundError:
     print('')
-
 copyfile('plot.dol','lastplot.plt')
 logFileName = stamp + '.log'
 fileLog = open (logFileName, 'at') 
@@ -38,31 +51,36 @@ fileLog.write("TEMP DECISION SECONDS\n")
 plotfileInjectLogfile = open ('lastplot.plt','at')
 plotfileInjectLogfile.write("plot '"+ logFileName +"' using 3:1, '' using 3:2 \n" + "pause -1" )
 
-minima = 29.00
-maxima = 33.00
+
+
+
+
 
 iterationCounter = 1
 
 while 1:  
 
     try:
+        #LÊ PORTA SERIAL E CONVERTE A STRING PRA FLOAT
         temp = ser.readline()
         leitura = str(temp)[2:7] 
         temperatura = float(temp)
      
+        #TOMA DECISÃO ENTRE TEMPERATURAS ESTABELECIDAS E MANDA RESPOSTA SERIAL
         if temperatura <= minima:
             decisao = "Alta"
             ser.write(b'H')
-     
         if temperatura  > minima :
             if  temperatura  <= maxima:
                 decisao = "Baixa"
                 ser.write(b'L')
-                             
         if temperatura > maxima:
             decisao = "Nula"
             ser.write(b'N') 
 
+        
+        
+        #CRIA TEXTO QUE IMPRIME NO CONSOLE E SALVA NO LOG
         decisaoOut = decisaoParaNumero(decisao)
         texto = str(leitura) + " " + str(decisaoOut) + " " + str(iterationCounter)  
         print (texto)
@@ -73,3 +91,7 @@ while 1:
           
     iterationCounter = iterationCounter + 1
     time.sleep(1)
+
+    #LIMITADOR DE TEMPO DE EXECUÇÃO DO EXPERIMENTO
+    if iterationCounter > tempoTotalEmSegundos(horasDeExperimento,minutosDeExperimento):
+        break
--- copytognuplot.bat
+++ copytognuplot.bat
@@ -1,4 +1,5 @@
 move *.log C:\gnuplot\bin\
+del C:\gnuplot\bin\lastplot.plt
 move lastplot.plt C:\gnuplot\bin\
 cd C:\gnuplot\bin\
 gnuplot.exe lastplot.plt
